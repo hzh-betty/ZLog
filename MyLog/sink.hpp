abfb4091c79073e30a4c6539878f967f052bf6c4
hzh-betty <beidi_99@outlook.com>
2025-03-02 00:10:22 +0800

    工厂模式设计多方式落地模块

---
+++ MyLog/sink.hpp
@@ -0,0 +1,191 @@
+#pragma once
+/*
+    日志落地模块的实现
+        1. 抽象落地基类
+        2. 派生子类
+        3. 使用工厂模式进行创建与表示的分离
+*/
+#include "util.hpp"
+#include <memory>
+#include <fstream>
+#include <cassert>
+#include <sstream>
+namespace mylog
+{
+    class LogSink
+    {
+    public:
+        using ptr = std::shared_ptr<LogSink>;
+        LogSink() {}
+        virtual ~LogSink() {}
+        virtual void log(const char *data, size_t len) = 0;
+    };
+
+    // 标准输出
+    class StdOutSink : public LogSink
+    {
+    public:
+        void log(const char *data, size_t len) override
+        {
+            std::cout.write(data, len);
+        }
+    };
+
+    // 指定文件
+    class FileSink : public LogSink
+    {
+    public:
+        FileSink(const std::string &pathname)
+            : pathname_(pathname)
+        {
+            // 1.创建日志文件所用的路径
+            File::createDiretory(File::path(pathname));
+
+            // 2. 创建并打开日志文件
+            ofs_.open(pathname_, std::ios::binary | std::ios::app);
+            assert(ofs_.is_open());
+        }
+        void log(const char *log, size_t len) override
+        {
+            ofs_.write(log, len);
+            assert(ofs_.good());
+        }
+
+    protected:
+        std::string pathname_;
+        std::ofstream ofs_;
+    };
+
+    // 滚动文件(以大小)
+    class RollBySizeSink : public LogSink
+    {
+    public:
+        RollBySizeSink(const std::string &basename, size_t maxSize)
+            : basename_(basename), maxSize_(maxSize), curSize_(0), nameCount(0)
+        {
+            // 1.创建日志文件所用的路径
+            std::string pathname = createNewFile();
+            File::createDiretory(File::path(pathname));
+
+            // 2. 创建并打开日志文件
+            ofs_.open(pathname, std::ios::binary | std::ios::app);
+            assert(ofs_.is_open());
+        }
+
+        void log(const char *log, size_t len) override
+        {
+            if (curSize_ >= maxSize_)
+            {
+                ofs_.close();
+                std::string pathname = createNewFile();
+                ofs_.open(pathname, std::ios::binary | std::ios::app);
+                curSize_ = 0;
+                assert(ofs_.is_open());
+            }
+            ofs_.write(log, len);
+            curSize_ += len;
+            assert(ofs_.good());
+        }
+
+    protected:
+        // 进行大小判断，超过就创建新文件
+        std::string createNewFile()
+        {
+            time_t t = Date::getCurrentTime();
+            struct tm lt;
+            localtime_r(&t, &lt);
+            std::stringstream filename;
+            filename << basename_ << (lt.tm_year + 1900) << (lt.tm_mon + 1) << lt.tm_mday
+                     << lt.tm_hour << lt.tm_min << lt.tm_sec << "-" << nameCount++ << ".log";
+            return filename.str();
+        }
+
+    protected:
+        std::string basename_; // 基础文件 + 扩展文件名
+        std::ofstream ofs_;
+        size_t maxSize_; // 最大文件大小
+        size_t curSize_;
+        size_t nameCount; // 名称计数器
+    };
+
+    //  滚动文件(以时间)
+    enum class TimeGap
+    {
+        GAP_SECOND,
+        GAP_MINUTE,
+        GAP_HOUR,
+        GAP_DAY,
+    };
+
+    class RollByTimeSink : public LogSink
+    {
+    public:
+        RollByTimeSink(const std::string &basename, TimeGap gaptype)
+            : basename_(basename)
+        {
+            switch (gaptype)
+            {
+            case TimeGap::GAP_SECOND:
+                gapSize_ = 1;
+                break;
+            case TimeGap::GAP_MINUTE:
+                gapSize_ = 60;
+                break;
+            case TimeGap::GAP_HOUR:
+                gapSize_ = 3600;
+                break;
+            case TimeGap::GAP_DAY:
+                gapSize_ = 3600 * 24;
+                break;
+            }
+
+            curGap_ = Date::getCurrentTime() / gapSize_;
+            std::string filename = createNewFile();
+            File::createDiretory(File::path(filename));
+            ofs_.open(filename, std::ios::binary | std::ios::app);
+            assert(ofs_.is_open());
+        }
+
+        void log(const char *log, size_t len) override
+        {
+            time_t cur = Date::getCurrentTime();
+            if (cur / gapSize_ != curGap_)
+            {
+                ofs_.close();
+                std::string pathname = createNewFile();
+                ofs_.open(pathname, std::ios::binary | std::ios::app);
+            }
+            ofs_.write(log, len);
+            assert(ofs_.good());
+        }
+
+    protected:
+        std::string createNewFile()
+        {
+            time_t t = Date::getCurrentTime();
+            struct tm lt;
+            localtime_r(&t, &lt);
+            std::stringstream filename;
+            filename << basename_ << (lt.tm_year + 1900) << (lt.tm_mon + 1) << lt.tm_mday
+                     << lt.tm_hour << lt.tm_min << lt.tm_sec << ".log";
+            return filename.str();
+        }
+
+    protected:
+        std::string basename_;
+        std::ofstream ofs_;
+        size_t curGap_;  // 当前是第几个时间段
+        size_t gapSize_; // 时间段的大小
+    };
+
+    // 简单工厂模式
+    class SinkFactory
+    {
+    public:
+        template <typename SinkType, typename... Args>
+        static LogSink::ptr create(Args &&...args)
+        {
+            return std::make_shared<SinkType>(std::forward<Args>(args)...);
+        }
+    };
+};
--- MyLog/test.cc
+++ MyLog/test.cc
@@ -2,6 +2,7 @@
 #include "level.hpp"
 #include "message.hpp"
 #include "format.hpp"
+#include "sink.hpp"
 #include <thread>
 
 int main()
@@ -19,7 +20,29 @@ int main()
 
     std::string file = "test.cc";
     mylog::LogMessage logmsg(mylog::LogLevel::value::INFO, file, 19, "这是一个测试", "master");
-    mylog::Formmatter fmt("abc%%abc[%d{%H-%M-%S]} %e%m%n");
-    std::cout << fmt.format(logmsg);
+    mylog::Formmatter fmt("abc%%abc[%d{%H:%M:%S]} %T%m%n");
+    std::string str = fmt.format(logmsg);
+    // mylog::LogSink::ptr stdout_ptr = mylog::SinkFactory::create<mylog::StdOutSink>();
+    // mylog::LogSink::ptr file_ptr = mylog::SinkFactory::create<mylog::FileSink>("./logfile/test.log");
+    // mylog::LogSink::ptr roll_ptr = mylog::SinkFactory::create<mylog::RollBySizeSink>("./logfile/roll-", 1024 * 1024);
+    // stdout_ptr->log(str.c_str(), str.size());
+    // file_ptr->log(str.c_str(), str.size());
+    // roll_ptr->log(str.c_str(), str.size());
+
+    // size_t curSize = 0;
+    // size_t cnt = 0;
+    // while (curSize < 1024 * 1024 * 10)
+    // {
+    //     std::string tmp = str + std::to_string(cnt++);
+    //     roll_ptr->log(tmp.c_str(), tmp.size());
+    //     curSize += tmp.size();
+    // }
+
+    mylog::LogSink::ptr time_ptr = mylog::SinkFactory::create<mylog::RollByTimeSink>("./logfile/roll-", mylog::TimeGap::GAP_SECOND);
+    time_t cur = mylog::Date::getCurrentTime();
+    while (mylog::Date::getCurrentTime() < cur + 5)
+    {
+        time_ptr->log(str.c_str(), str.size());
+    }
     return 0;
 }
